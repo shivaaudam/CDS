cb018573d5cda5ba172ae2c22ccf363e187f44ea
roop <yc05rp2@macys.com>
2015-03-12 18:58:13 +0530

    added code for loading modules using requireJS

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,55 @@
 /*global define*/
 'use strict';
 
-define(['angular'], function (angular) {
-	return angular.module('CDS', []);
+define(['angular','angularRoute','vendor/routeResolver'], function (angular) {
+
+	
+	var app = angular.module('CDS', ['ngRoute','routeResolverServices']);
+
+	app.config(['$routeProvider', 'routeResolverProvider', '$controllerProvider', '$compileProvider', '$filterProvider', '$provide',
+        function ($routeProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide) {
+
+
+        	routeResolverProvider.routeConfig.setBaseDirectories('scripts/views', 'scripts/controllers');
+
+        	app.register =
+            {
+                controller: $controllerProvider.register,
+                directive: $compileProvider.directive,
+                filter: $filterProvider.register,
+                factory: $provide.factory,
+                service: $provide.service
+            };
+             //Define routes - controllers will be loaded dynamically
+            var route = routeResolverProvider.route;
+
+            $routeProvider.when('/customers', route.resolve('Customers'))
+            	.when('/customerorders/:customerID', route.resolve('CustomerOrders'))
+                .otherwise({ redirectTo: '/customers' });
+
+
+                }]);
+
+      /* app.run(['$rootScope', '$location', 'authService',
+        function ($rootScope, $location, authService) {
+            
+            //Client-side security. Server-side framework MUST add it's 
+            //own security as well since client-based security is easily hacked
+            $rootScope.$on("$routeChangeStart", function (event, next, current) {
+                if (next && next.$$route && next.$$route.secure) {
+                    if (!authService.user.isAuthenticated) {
+                        $rootScope.$evalAsync(function () {
+                            authService.redirectToLogin();
+                        });
+                    }
+                }
+            });
+
+    }]); */
+
+
+
+	//app.provider('routeResolver', routeResolver);
+
+	return app;
 });
