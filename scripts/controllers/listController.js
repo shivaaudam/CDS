cbd685467445117120c52503931bc7e7ad2a4594
unknown <rpadala@RPADALA.nisum.com>
2015-03-17 18:51:34 +0530

    updated tiles display

--- scripts/controllers/listController.js
+++ scripts/controllers/listController.js
@@ -5,13 +5,20 @@ define(['app'], function (app) {
 
 	 app.register.controller('listController', ['$scope','$http', function($scope,$http){
 
+	 	$scope.filter = false;
+
 		$http.get('constituencyData.json').success(function(resp){
-	   		console.log(resp);
+	   	
             $scope.constituencyData = resp;
 	   	}); 
+
 	   	$scope.add = function(){
 			console.log("here");
 		} 
+
+		$scope.showFilter = function(){
+			$scope.filter = !$scope.filter;
+		}
 
 	}]);
 
